5a948d95926450c408b816b2c9654192e7a32329
AH-Ratul <ahratul21@gmail.com>
2025-10-07 18:13:04 +0600

    feat: add middlewares, globalErrorHandler, base route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express, { Request, Response } from "express";
+import { appRouter } from "./app/route";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 export const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/api/v1", appRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("My Portfolio is running");
 });
+
+app.use(globalErrorHandler);
